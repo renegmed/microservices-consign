037107d35847af17f831094b22844767d709ee6c
ren medal <rmedal@pop-os.localdomain>
2019-11-13 10:54:03 -0500

    added get consignment list

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -8,9 +8,9 @@ import (
 	"sync"
 
 	// Import the generated protobuf code
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	pb "microservices-consign/proto/consignment"
+
+	"google.golang.org/grpc"
 )
 
 const (
@@ -19,6 +19,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -35,6 +36,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -61,6 +67,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
+// GetConsignments -
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
 func main() {
 	repo := &Repository{}
 
@@ -77,7 +89,7 @@ func main() {
 	pb.RegisterShippingServiceServer(s, &service{repo})
 
 	// Register reflection service on gRPC server.
-	reflection.Register(s)
+	//reflection.Register(s)
 
 	log.Println("Running on port:", port)
 	if err := s.Serve(lis); err != nil {
